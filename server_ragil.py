2c97ac91670fbf7cebb5a860860c08b51b0c7831
Elad <eladru1@gmail.com>
2020-02-05 20:03:53 +0200

    Initial Server

--- server_ragil.py
+++ server_ragil.py
@@ -1,18 +1,25 @@
 from threading import Thread
 import socket
 
+
 def accept1(socket, lst_of_clients):
     while True:
         obj, add = socket.accept()
         lst_of_clients.append(obj)
 
-def recv(address,massage):
+
+def recv(address, massage):
     massage = address.recv()
+
 
 def send(lst_of_clients):
     for i in lst_of_clients:
         massage = b""
-        thread = Thread(target=recv, args=(i,massage))
+        thread = Thread(target=recv, args=(i, massage))
+
+
+import socket
+import select
 
 
 def main():
@@ -20,8 +27,11 @@ def main():
     host.bind(("127.0.0.7", 1234))
     host.listen(2)
     lst = []
+    all_connected_r = [host]
 
-    thread = Thread(target=accept1, args=(host, lst))
+    while True:
+        readable, writable, xable = select.select(all_connected_r, [], [])
 
-
-
+        for r in readable:
+            if r is host:
+                all_connected_r.append(r.accept())
